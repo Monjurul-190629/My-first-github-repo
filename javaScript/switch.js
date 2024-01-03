35d5b27a77a35729d38409db01e2ffc208ea40a3
Monjurul <monjurulalam190629@gmail.com>
2024-01-03 19:22:07 +0600

    another file is added

---
+++ javaScript/switch.js
@@ -0,0 +1,13 @@
+var x = 2
+
+switch(x){
+    case 1:
+        console.log('yes! you have done it');
+        break;
+    case 2:
+        console.log("no, another try id needed")
+        break;
+    default:
+        console.log('Nothing')
+    }
+
---
+++ math.js
@@ -0,0 +1,5 @@
+var s = 123
+var i = 12
+
+var j = (s + i) < 255 ? (s+i) : 'none';
+console.log(j)
